6c43a8bca9723f959141d9da6cc291dc11ff107f
unknown <albab9883@gmail.com>
2026-03-14 01:25:53 +0700

    fix header mobile

--- src/routes/chat/+page.svelte
+++ src/routes/chat/+page.svelte
@@ -349,7 +349,7 @@
 
 <svelte:head><title>Chat</title></svelte:head>
 
-<div class="flex h-[100dvh] w-full overflow-hidden bg-white">
+<div class="fixed inset-0 w-full flex bg-white overflow-hidden">
 	<!-- SIDEBAR -->
 	<aside
 		class="
